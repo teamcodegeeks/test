d41fca8a06ca934806c0b39cd9e2585b5b8fc7ce
noblebethel <noblebethel@gmail.com>
2019-07-09 14:24:31 -0400

    first commit

---
+++ WebApplication5/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models
+{
+    public class Customer
+    {
+        [Required]
+        [RegularExpression("^[A-Z]{3,3}[0-9]{4,4}$")]
+        public string CustomerCode { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string CustomerName { get; set; }
+    }
+}
